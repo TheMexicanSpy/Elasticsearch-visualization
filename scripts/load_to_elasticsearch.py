f7a1c7e811c27efaba64a3800c34470377b260ef
TheMexicanSpy <147427136+TheMexicanSpy@users.noreply.github.com>
2025-05-11 11:18:12 -0500

    Update load_to_elasticsearch.py

--- scripts/load_to_elasticsearch.py
+++ scripts/load_to_elasticsearch.py
@@ -6,8 +6,8 @@ import os
 def load_data_to_elasticsearch():
     # Configuración de Elasticsearch
     es = Elasticsearch(
-        cloud_id="tu-cloud-id",  # Reemplazar con tu cloud ID
-        http_auth=("elastic", "tu-password")  # Reemplazar con tus credenciales
+        cloud_id="github_actions:ea01be305c7a4e50871237b16bde813b:dXMtY2VudHJhbDEuZ2NwLmNsb3VkLmVzLmlvJDJiZjliNjZkYWI2YjRlYTY5MWFiMmY1ODMzOGZmNGRjJDQ5NTE2ZTE0NGFhZTQzZjVhZTc5Y2NmMzlkZjg3Y2Iy",  # Reemplazar con tu cloud ID
+        http_auth=("briceno", "Luigi123@")  # Reemplazar con tus credenciales
     )
     
     # Leer el dataset
